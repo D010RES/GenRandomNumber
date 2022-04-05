717d65202ffb1e4d38bc6ad2213c7277df0ff8c1
D010RES <83914191+d010res@users.noreply.github.com>
2022-04-06 01:24:02 +0430

    initial-commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.chrtRandomNumber = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chrtRandomNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCount
+            // 
+            this.txtCount.Location = new System.Drawing.Point(81, 19);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(213, 22);
+            this.txtCount.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Count :";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(438, 36);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(86, 25);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 126);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(202, 340);
+            this.listBox1.TabIndex = 6;
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(82, 54);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(213, 22);
+            this.txtMin.TabIndex = 7;
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(82, 85);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(212, 22);
+            this.txtMax.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Min :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Max : ";
+            // 
+            // chrtRandomNumber
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chrtRandomNumber.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chrtRandomNumber.Legends.Add(legend1);
+            this.chrtRandomNumber.Location = new System.Drawing.Point(229, 126);
+            this.chrtRandomNumber.Name = "chrtRandomNumber";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.LegendText = "تعداد تکرار";
+            series1.Name = "Rep";
+            this.chrtRandomNumber.Series.Add(series1);
+            this.chrtRandomNumber.Size = new System.Drawing.Size(678, 342);
+            this.chrtRandomNumber.TabIndex = 11;
+            this.chrtRandomNumber.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 480);
+            this.Controls.Add(this.chrtRandomNumber);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chrtRandomNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chrtRandomNumber;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
+        }
+        private void btnGenerate_Click(object sender, EventArgs e) {
+            long randomTick, result;
+            int min, max;
+            listBox1.Items.Clear();
+            List<long> myList = new List<long>();
+            min = int.Parse(txtMin.Text);
+            max = int.Parse(txtMax.Text);
+            int count = int.Parse(txtCount.Text.ToString());
+            for (int i = 1; i < count; i++) {
+            runAgaing:
+                randomTick = DateTime.Now.Millisecond;
+                randomTick = randomTick % max;
+                randomTick = (9463 * randomTick + 5461) % 233280;
+                result = randomTick % max + min;
+                if (!(result > min && result < max)) {
+                    goto runAgaing;
+                }
+                myList.Add(result);
+                Thread.Sleep(1);
+            }
+            foreach (var item in myList) {
+                listBox1.Items.Add(item);
+            }
+            fillChart(myList);
+        }
+        private void fillChart(List<long> numbers) {
+            CleanChart();
+            var myDict = new Dictionary<long, int>();
+            
+            foreach (var number in numbers) {
+                int count = numbers.Count(i=> i == number);
+                myDict[number] = count;   
+            }
+
+            foreach(KeyValuePair<long, int> entry in myDict) {
+               chrtRandomNumber.Series["Rep"].Points.AddXY(entry.Key, entry.Value);
+            } 
+        }
+
+        private void CleanChart() { 
+            foreach (var series in chrtRandomNumber.Series) {
+                series.Points.Clear();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e) {
+            txtCount.Text = "30";
+            txtMax.Text = "100000";
+            txtMin.Text = "50000";
+            chrtRandomNumber.Titles.Add("تعداد دفعات تکرار اعداد تصادفی");
+
+        }
+        private void fontDialog1_Apply(object sender, EventArgs e) {
+
+        }
+    }
+}
